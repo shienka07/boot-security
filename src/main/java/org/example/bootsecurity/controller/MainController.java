661ef3139050fa2db1dab29b3d90ca330273a11e
JO KYEONGHYE <shienka07.ko@gmail.com>
2025-04-10 10:42:33 +0900

    thymeleaf 작동

---
+++ src/main/java/org/example/bootsecurity/controller/MainController.java
@@ -0,0 +1,15 @@
+package org.example.bootsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("message", "Hello World!");
+        return "index";
+    }
+}
